ac0d33e8e39780ac36b70d744200725735c3ed3a
CrhistianMRe <came.29meca@gmail.com>
2026-05-17 11:04:30 +0800

    feat(CH14): add complete solved L9

---
+++ CH14/L9/main.py
@@ -0,0 +1,27 @@
+class Graph:
+    def unconnected_vertices(self):
+        list = []
+        for key in self.graph:
+            if(self.graph.get(key) == set()):
+                list.append(key)
+        return list
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u in self.graph:
+            self.graph[u].add(v)
+        else:
+            self.graph[u] = {v}
+        if v in self.graph:
+            self.graph[v].add(u)
+        else:
+            self.graph[v] = {u}
+
+    def add_node(self, u):
+        if u not in self.graph:
+            self.graph[u] = set()
+
