359f572de9e9049c1b6004eae45861f3f3ccc248
CrhistianMRe <came.29meca@gmail.com>
2026-08-12 15:42:04 +0800

    feat(CH2): update L7 completed problem

--- CH2/possible_orders/main.py
+++ CH2/possible_orders/main.py
@@ -1,7 +1,9 @@
-def num_possible_orders(num_posts):
-    sum = 1
-    for i in range(num_posts+1):
-        if i !=0:  
-            sum *= i
+def num_possible_orders(num_posts: int) -> int:
+    total = 1
 
-    return sum
+    for i in range(num_posts, 1, -1):
+        total *= i 
+
+    return total
+
+
