a889c7f19c1a9ebb69557e90fd42d3621f6a4a14
CrhistianMRe <came.29meca@gmail.com>
2026-08-12 15:54:44 +0800

    feat(CH2): update L10 completed problem

--- CH2/log_scale/main.py
+++ CH2/log_scale/main.py
@@ -1,9 +1,12 @@
-import math 
+import math
 
-def log_scale(data, base):
-    list = []
-    for i in data:
-        list.append(math.log(i, base))
-    return list
-        
+
+def log_scale(data: list[float], base: float) -> list[float]:
+    resultList = []
+
+    for num in data:
+        resultList.append(math.log(num, base))
+    
+    return resultList
+
 
