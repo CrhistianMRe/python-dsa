03d4cea2697ad3e0ac37bf30f681089203a63352
CrhistianMRe <came.29meca@gmail.com>
2026-08-12 11:58:55 +0800

    feat(CH2): update L5 completed problem

--- CH2/influencer_score/main.py
+++ CH2/influencer_score/main.py
@@ -1,11 +1,8 @@
 import math
 
 
-def get_influencer_score(num_followers, average_engagement_percentage):
-    influencer_score = None 
-    if num_followers != 0 and average_engagement_percentage != 0:
-        influencer_score = average_engagement_percentage *(math.log(num_followers, 2))
-
-    return influencer_score
-
+def get_influencer_score(
+    num_followers: int, average_engagement_percentage: float
+) -> float:
+    return average_engagement_percentage * math.log(num_followers, 2)
 
