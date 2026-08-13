6c283d792506652b353ddecadfd4e30e2af10ba2
CrhistianMRe <came.29meca@gmail.com>
2026-08-13 18:00:19 +0800

    feat(CH3): add L5 updated problem

--- CH3/avg_brand_followers/main.py
+++ CH3/avg_brand_followers/main.py
@@ -1,11 +1,20 @@
-def get_avg_brand_followers(all_handles, brand_name):
-    count = 0
-    list_count = 0
-    for list in all_handles:
-        list_count += 1
-        for string in list:
-            if(string.find(brand_name) != -1): count += 1
-    return count / list_count 
+def get_avg_brand_followers(all_handles: list[list[str]], brand_name: str) -> float:
+    handle_amount = 0
+
+    parentListLength = len(all_handles)
+
+    for i in range(0, parentListLength):
+        subList = all_handles[i]
+        subListLength = len(subList)
+        for a in range(0, subListLength):
+            if brand_name in subList[a]:
+                handle_amount += 1
+
+
+    print(parentListLength)
+
+    return handle_amount / parentListLength
+            
 
 
 
