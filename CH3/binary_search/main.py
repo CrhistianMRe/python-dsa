3b4292b67ba518b381c6f1befaf805cab1e226ee
CrhistianMRe <came.29meca@gmail.com>
2026-08-18 12:15:26 +0800

    feat(CH3): add updated L9 problem

--- CH3/binary_search/main.py
+++ CH3/binary_search/main.py
@@ -1,15 +1,17 @@
-def binary_search(target, arr):
+def binary_search(target: int, arr: list[int]) -> bool:
     low = 0
-    high = len(arr)-1
-    median = None
-    while low <= high:
+    high = len(arr) - 1
+
+    while(low <= high):
+
         median = (low + high) // 2
-        if arr[median] == target: 
-            return True 
-        elif arr[median] < target: 
-            low = median + 1
+
+        if(arr[median] == target):
+            return True
+        elif arr[median] < target:
+             low = median + 1
         else:
             high = median - 1
-    return False 
 
+    return False
 
