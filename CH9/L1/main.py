feadb12ff2aaf8fb7103d76b870028682ca161ec
CrhistianMRe <came.29meca@gmail.com>
2026-09-05 09:32:59 +0800

    feat(CH9): add updated L1 problem

--- CH9/L1/main.py
+++ CH9/L1/main.py
@@ -1,13 +1,18 @@
+from typing import Any
+
+
 class Node:
-    def __init__(self, val):
+    val: Any
+
+    def __init__(self, val: Any) -> None:
         self.val = val
         self.next = None
 
-    def set_next(self, node):
+    def set_next(self, node: "Node") -> None:
         self.next = node
 
     # don't touch below this line
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.val
 
--- CH9/L1/main_test.py
+++ CH9/L1/main_test.py
@@ -1,13 +1,17 @@
-from main import *
+import pytest
+from main import Node
 
 run_cases = [
-    ("Anton Chigurh", ["Llewelyn Moss", "Anton Chigurh"]),
-    ("Carson Wells", ["Llewelyn Moss", "Anton Chigurh", "Carson Wells"]),
-    ("Ed Tom Bell", ["Llewelyn Moss", "Anton Chigurh", "Carson Wells", "Ed Tom Bell"]),
+    pytest.param("Anton Chigurh", ["Llewelyn Moss", "Anton Chigurh"]),
+    pytest.param("Carson Wells", ["Llewelyn Moss", "Anton Chigurh", "Carson Wells"]),
+    pytest.param(
+        "Ed Tom Bell",
+        ["Llewelyn Moss", "Anton Chigurh", "Carson Wells", "Ed Tom Bell"],
+    ),
 ]
 
-submit_cases = run_cases + [
-    (
+submit_cases = [
+    pytest.param(
         "Carla Jean Moss",
         [
             "Llewelyn Moss",
@@ -16,8 +20,9 @@ submit_cases = run_cases + [
             "Ed Tom Bell",
             "Carla Jean Moss",
         ],
+        marks=pytest.mark.submit,
     ),
-    (
+    pytest.param(
         "Wendell",
         [
             "Llewelyn Moss",
@@ -27,28 +32,28 @@ submit_cases = run_cases + [
             "Carla Jean Moss",
             "Wendell",
         ],
+        marks=pytest.mark.submit,
     ),
 ]
 
 
-def test(linked_list, input, expected_state):
-    print("---------------------------------")
+@pytest.fixture(scope="module")
+def linked_list():
+    return Node("Llewelyn Moss")
+
+
+@pytest.mark.parametrize(("input", "expected_state"), run_cases + submit_cases)
+def test_set_next(linked_list, input, expected_state):
+    print("\n---------------------------------")
     print(f"Linked List: {linked_list_to_str(linked_list)}")
     print(f"Set Next: {input}")
     print(f"Expected: {expected_state}")
     node = Node(input)
     last_node = get_last_node(linked_list)
     last_node.set_next(node)
-    try:
-        result = linked_list_to_list(linked_list)
-    except Exception as e:
-        result = f"Error: {e}"
+    result = linked_list_to_list(linked_list)
     print(f"Actual: {result}")
-    if result == expected_state:
-        print("Pass")
-        return True
-    print("Fail")
-    return False
+    assert result == expected_state
 
 
 def linked_list_to_list(node):
@@ -73,34 +78,5 @@ def linked_list_to_str(node):
     while current and hasattr(current, "val"):
         linked_list_str += current.val + " -> "
         current = current.next
-
     return linked_list_str
-
-
-def main():
-    passed = 0
-    failed = 0
-    linked_list = Node("Llewelyn Moss")
-    skipped = len(submit_cases) - len(test_cases)
-    for test_case in test_cases:
-        correct = test(linked_list, *test_case)
-        if correct:
-            passed += 1
-        else:
-            failed += 1
-    if failed == 0:
-        print("============= PASS ==============")
-    else:
-        print("============= FAIL ==============")
-    if skipped > 0:
-        print(f"{passed} passed, {failed} failed, {skipped} skipped")
-    else:
-        print(f"{passed} passed, {failed} failed")
-
-
-test_cases = submit_cases
-if "__RUN__" in globals():
-    test_cases = run_cases
-
-main()
 
