5415604c27f7d5897adedad42a1ee3636a749d75
CrhistianMRe <came.29meca@gmail.com>
2026-09-05 10:10:47 +0800

    feat(CH9): add updated L4 problem

--- CH9/L4/main.py
+++ CH9/L4/main.py
@@ -1,3 +1,25 @@
-def backoff_delays(base: int):
-    pass
+from node import Node
+
+
+class LinkedList:
+    head: Node | None
+
+    def __init__(self) -> None:
+        self.head = None
+
+    def __iter__(self):
+        temp = self.head
+        while(not (temp == None)):
+            yield temp
+            temp = temp.next
+
+    # don't touch below this line
+
+    def __repr__(self) -> str:
+        nodes = []
+        current = self.head
+        while current and hasattr(current, "val"):
+            nodes.append(current.val)
+            current = current.next
+        return " -> ".join(nodes)
 
